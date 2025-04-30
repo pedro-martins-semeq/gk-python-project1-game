1f060a12e15a4b515d9c194aaadc594b56654811
Pedro Martins <pedro.martins@semeq.com>
2025-04-30 15:37:24 -0300

    Calculator and Game class implementation of minimal game - WIP.

--- game.py
+++ game.py
@@ -1,2 +1,89 @@
+from os import name as _os_name, system as _os_system
+from models.calculator import Calculator
+
+
+def clear() -> None:
+    _os_system("cls" if _os_name == "nt" else "clear")
+
+
+class Game:
+    def __init__(self, calculator: Calculator):
+        self.__calculator = calculator
+        self.__score = 0
+        self.__report = str(self)
+
+    def __str__(self) -> str:
+        return (
+            f"Game - Score: {self.get_score}\n"
+            + f"Calculator: \n{str(self.__calculator)}"
+        )
+
+    @property
+    def get_score(self) -> int:
+        return self.__score
+
+    def get_report(self) -> str:
+        return self.__report
+
+    def answer(self, answer: str) -> bool:
+        if self._is_correct_answer(answer):
+            self._score()
+            self.__report = "CORRECT ANSWER\n" + self.__report
+            return True
+        else:
+            self.__report = (
+                "WRONG ANSWER "
+                + f"(expected {self.__calculator.result}))\n"
+                + self.__report
+                + "GAME OVER\n"
+            )
+            self._game_over()
+            return False
+
+    def get_str_expression(self):
+        return (
+            f"{self.__calculator.value1} "
+            + f"{self.__calculator.operation_symbol} "
+            + f"{self.__calculator.value2} = "
+        )
+
+    def update_report(self) -> None:
+        self.__report = str(self)
+
+    def _score(self) -> None:
+        self.__score += 1
+        self.__calculator.difficulty += 1
+
+    def _game_over(self) -> None:
+        self.__calculator = Calculator(1)
+        self.__score = 0
+
+    def _is_correct_answer(self, answer: str) -> bool:
+        return self.__calculator.result == float(answer)
+
+
+def play():
+    calculator: Calculator = Calculator(1)
+
+    game: Game = Game(calculator)
+
+    clear()
+
+    while True:
+        clear()
+        game.update_report()
+
+        print(f"Score: {game.get_score}")
+        answer: str = input(game.get_str_expression())
+        game.answer(answer)
+
+        print(game.get_report())
+        input("Press <enter> to continue...")
+
+
+def main():
+    play()
+
+
 if __name__ == "__main__":
-    print("Hello, World!")
+    main()
--- models/calculator.py
+++ models/calculator.py
@@ -1,16 +1,33 @@
 from random import randint
+from typing import Dict
 
 
 class Calculator:
-    __operations_sym_dict = {1: "+", 2: "-", 3: "*"}
-    __operations_name_dict = {1: "add", 2: "sub", 3: "mult"}
+    __operations_sym_dict: Dict[int, str] = {1: "+", 2: "-", 3: "*"}
+    __operations_name_dict: Dict[int, str] = {1: "add", 2: "sub", 3: "mult"}
 
     def __init__(self, difficulty: int) -> None:
         self.__difficulty: int = difficulty
-        self.__value1: float = self._generate_value()
-        self.__value2: float = self._generate_value()
-        self.__operation: int = randint(1, len(self.__operations_sym_dict))
-        self.__result: float = self._generate_result()
+        self.__value1: float = self._calculate_new_value()
+        self.__value2: float = self._calculate_new_value()
+        self.__operation: int = self._get_new_operation()
+        self.__result: float = self._calculate_result()
+
+    def __str__(self) -> str:
+        return (
+            f"Value 1: {self.value1}\n"
+            + f"Value 2: {self.value2}\n"
+            + f"Difficulty: {self.difficulty}\n"
+            + f"Operation: {self.operation_symbol} ({self.operation_text})\n"
+        )
+
+    def __repr__(self) -> str:
+        return (
+            f"Calculator class <object> - value1:{self.value1} "
+            + f"value2: {self.value2} "
+            + f"difficulty:{self.difficulty} "
+            + f"operation: {self.operation}"
+        )
 
     @property
     def difficulty(self) -> int:
@@ -40,31 +57,29 @@ class Calculator:
     def result(self) -> float:
         return self.__result
 
-    def _generate_value(self) -> float:
+    @difficulty.setter
+    def difficulty(self, difficulty: int) -> None:
+        self.__difficulty = difficulty
+        self._generate_new_operation()
+
+    def _generate_new_operation(self) -> None:
+        self.__value1 = self._calculate_new_value()
+        self.__value2 = self._calculate_new_value()
+        self.__operation = self._get_new_operation()
+        self.__result = self._calculate_result()
+
+    def _calculate_new_value(self) -> float:
         return randint(0, pow(self.difficulty, 2))
 
-    def _generate_result(self) -> float:
+    def _calculate_result(self) -> float:
         if self.operation == 1:
-            return self.value1 + self.value2
+            return self.value1 + self.value2  # Addition
         elif self.operation == 2:
-            return self.value1 - self.value2
+            return self.value1 - self.value2  # Subtraction
         elif self.operation == 3:
-            return self.value1 * self.value2
+            return self.value1 * self.value2  # Multiplication
         else:
             return 0.0
 
-    def __str__(self) -> str:
-        return (
-            f"Value 1: {self.value1}\n"
-            + f"Value 2: {self.value2}\n"
-            + f"Difficulty: {self.difficulty}\n"
-            + f"Operation: {self.operation_symbol} ({self.operation_text})\n"
-        )
-
-    def __repr__(self) -> str:
-        return (
-            f"Calculator class <object> - value1:{self.value1} "
-            + f"value2: {self.value2} "
-            + f"difficulty:{self.difficulty} "
-            + f"operation: {self.operation}"
-        )
+    def _get_new_operation(self) -> int:
+        return randint(1, len(self.__operations_sym_dict))
